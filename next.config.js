433acff5fab09578825689eeb70bc763c7090f7f
emiliebunny <emilie>
2024-08-11 09:07:07 +0000

    try re-build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    basePath: '/posts-next',
+  },
   basePath: '/posts-next',
   output: 'export',
   distDir: 'docs',
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
